9402c1193fe4ddfe18c0fd55f85ff33af427a927
Vladislav Zedano <zedano.vladislav@gmail.com>
2020-11-28 00:18:33 -0500

    Commit transitorio.

--- reto.py
+++ reto.py
@@ -16,9 +16,29 @@ sales_file_path = config['data-files']['sales']
 sales = pd.read_csv(sales_file_path, encoding='latin-1')
 executed_promos = pd.read_csv(executed_promos_file_path, encoding='latin-1')
 active_promos = pd.read_csv(active_promos_file_path, encoding='latin-1')
-client_attributes = pd.read_csv(
+clients_attributes = pd.read_csv(
     client_attributes_file_path, encoding='latin-1')
 
-print(sales)
+sanitized_executed_promos = executed_promos.drop_duplicates(
+    subset=['Cliente', 'Marca', 'Cupo'])
 
-active_promos.to_csv('test.csv', index=False)
+sales_exec_promos = sales.merge(sanitized_executed_promos, how='left', on=[
+                                'Cliente', 'Marca', 'Cupo'])
+
+sales_exec_promos.loc[
+    sales_exec_promos['CodigoDC'].isnull() == False, 'ES_PROMO'] = 1
+
+
+# This is the final dataframe we will analyze as part of the challenge
+df = sales_exec_promos.merge(clients_attributes, how='left', on='Cliente')
+
+del df['CodigoDC']
+del df['FechaAltaCliente']
+
+df = df.fillna({
+    'ES_PROMO': 0,
+    'SegmentoPrecio': df['SegmentoPrecio'].mode()[0],
+    'Cupo': df['Cupo'].mode()[0]
+})
+
+df
